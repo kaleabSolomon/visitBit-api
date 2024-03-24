223e9f7365d9938bea08b496a7d30c2641a14c3c
Kaleab Solomon <kaleabslmn@gmail.com>
2024-03-24 16:36:38 +0300

    implement the get location route

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Location = require("../models/locationModel");
+const AppError = require("../utils/AppError");
 
 exports.getAllLocations = asyncHandler(async (req, res) => {
   try {
@@ -20,6 +21,23 @@ exports.getAllLocations = asyncHandler(async (req, res) => {
     });
   }
 });
+
+exports.getLocation = asyncHandler(async (req, res) => {
+  try {
+    const location = await Location.findById(req.params.id);
+    if (!location) {
+      res.status(404).json(new AppError("location not found"));
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: { location },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(AppError("Unable to create the Location", 400));
+  }
+});
 exports.createLocation = asyncHandler(async (req, res) => {
   try {
     const location = await Location.create({
@@ -38,10 +56,6 @@ exports.createLocation = asyncHandler(async (req, res) => {
     });
   } catch (err) {
     console.log(err);
-    res.status(400).json({
-      status: "fail",
-      message: "Failed to create location",
-      error: err.message,
-    });
+    res.status(400).json(new AppError("Unable to create the Location", 400));
   }
 });
--- routes/locationRoutes.js
+++ routes/locationRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   getAllLocations,
   createLocation,
+  getLocation,
 } = require("../controllers/locationController");
 
 const router = express.Router();
