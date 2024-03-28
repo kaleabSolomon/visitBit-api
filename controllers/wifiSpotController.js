202d0918fe8da56cb1a892a733182a55afa1eb2b
Kaleab Solomon <kaleabslmn@gmail.com>
2024-03-29 01:28:09 +0300

    implement update and delete for wifi spot

--- controllers/wifiSpotController.js
+++ controllers/wifiSpotController.js
@@ -56,3 +56,40 @@ exports.createWifiSpots = asyncHandler(async (req, res) => {
     throw new AppError("Unable to create the location");
   }
 });
+
+exports.updateWifiSpot = asyncHandler(async (req, res) => {
+  const wifiSpot = await WifiSpot.findById(req.params.id);
+
+  if (!wifiSpot) {
+    res.status(404);
+    throw new AppError("Wifi spot not found");
+  }
+  try {
+    const updatedWifiSpot = await WifiSpot.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json({ status: "success", data: { updatedWifiSpot } });
+  } catch (err) {
+    res.status(400);
+    throw new AppError("Unable to update WifiSpot");
+  }
+});
+
+exports.deleteWifiSpot = asyncHandler(async (req, res) => {
+  const WifiSpot = await WifiSpot.findById(req.params.id);
+
+  if (!wifiSpot) {
+    res.json(new AppError("Wifi spot not found", 404));
+  }
+  try {
+    await wifiSpot.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({ status: "success", data: null });
+  } catch {
+    res.status(400);
+    throw new AppError("Unable to delete the wifi spot");
+  }
+});
--- routes/wifiSpotRoutes.js
+++ routes/wifiSpotRoutes.js
@@ -4,10 +4,16 @@ const {
   getWifiSpots,
   createWifiSpots,
   getWifiSpot,
+  updateWifiSpot,
+  deleteWifiSpot,
 } = require("../controllers/wifiSpotController");
 const router = express.Router();
 
 router.route("/").get(getWifiSpots).post(createWifiSpots);
-router.route("/:id").get(getWifiSpot);
+router
+  .route("/:id")
+  .get(getWifiSpot)
+  .patch(updateWifiSpot)
+  .delete(deleteWifiSpot);
 
 module.exports = router;
