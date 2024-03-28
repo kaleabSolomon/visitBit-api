d2ce8569bb0c460ecdeeb8cf3ddd314db9ce50b8
Kaleab Solomon <kaleabslmn@gmail.com>
2024-03-28 15:56:04 +0300

    implement the get wifi spot route

--- controllers/wifiSpotController.js
+++ controllers/wifiSpotController.js
@@ -18,6 +18,26 @@ exports.getWifiSpots = asyncHandler(async (req, res) => {
   }
 });
 
+exports.getWifiSpot = asyncHandler(async (req, res) => {
+  const wifiSpot = await WifiSpot.findById(req.params.id);
+
+  if (!wifiSpot) {
+    throw new AppError("WifiSpot not found");
+  }
+
+  try {
+    res.status(200).json({
+      status: "success",
+      data: {
+        wifiSpot,
+      },
+    });
+  } catch (err) {
+    res.status(400);
+    throw new AppError("Unable to fetch WifiSpot");
+  }
+});
+
 exports.createWifiSpots = asyncHandler(async (req, res) => {
   res.status(201).json({
     message: "creation success",
--- routes/wifiSpotRoutes.js
+++ routes/wifiSpotRoutes.js
@@ -3,9 +3,11 @@ const express = require("express");
 const {
   getWifiSpots,
   createWifiSpots,
+  getWifiSpot,
 } = require("../controllers/wifiSpotController");
 const router = express.Router();
 
 router.route("/").get(getWifiSpots).post(createWifiSpots);
+router.route("/:id").get(getWifiSpot);
 
 module.exports = router;
