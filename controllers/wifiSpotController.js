9e51b54c8f5bc2e92631a0b811c2411295cfbdd6
Kaleab Solomon <kaleabslmn@gmail.com>
2024-03-28 15:50:31 +0300

    implement the get wifispots route

--- controllers/wifiSpotController.js
+++ controllers/wifiSpotController.js
@@ -1,9 +1,21 @@
 const asyncHandler = require("express-async-handler");
-
+const WifiSpot = require("../models/wifiSpotModel");
+const AppError = require("../utils/AppError");
 exports.getWifiSpots = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "success",
-  });
+  try {
+    const wifiSpots = await WifiSpot.find();
+
+    res.status(200).json({
+      status: "success",
+      results: wifiSpots.length,
+      data: {
+        wifiSpots,
+      },
+    });
+  } catch (err) {
+    res.status(400);
+    throw new AppError("Failed to get wifi spots");
+  }
 });
 
 exports.createWifiSpots = asyncHandler(async (req, res) => {
